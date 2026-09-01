03efbf3cf96ed0c2ad7cd0519c0dcaa12bfd49d0
waxew <4622.ar@gmail.com>
2026-09-01 16:08:10 +0330

    fix: include shared Material icons in viewer app

--- academy-viewer/build.gradle.kts
+++ academy-viewer/build.gradle.kts
@@ -39,6 +39,7 @@ dependencies {
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.foundation:foundation")
     implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.material:material-icons-extended")
 
     debugImplementation("androidx.compose.ui:ui-tooling")
 }
