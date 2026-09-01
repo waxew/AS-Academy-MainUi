e2ae2dd11bb49ad7bbe95a2cfa7223837cfe42ac
waxew <4622.ar@gmail.com>
2026-09-01 16:05:21 +0330

    feat: enable Android application builds for MainCourse viewer

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     // Versions intentionally track the current AS-Academy-Core toolchain.
+    id("com.android.application") version "9.2.1" apply false
     id("com.android.library") version "9.2.1" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.4.10" apply false
 }
