ae9fef728b45697070515c9185b19817a66da84d
waxew <4622.ar@gmail.com>
2026-09-01 13:43:32 +0330

    build: define MainUi module coordinates

---
+++ build.gradle.kts
@@ -0,0 +1,10 @@
+plugins {
+    // Versions intentionally track the current AS-Academy-Core toolchain.
+    id("com.android.library") version "9.2.1" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.4.10" apply false
+}
+
+allprojects {
+    group = "com.asdevelopers.academy"
+    version = "0.1.0"
+}
