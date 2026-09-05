8e82cd2492a5285e23a228f426184faf68146f8f
waxew <4622.ar@gmail.com>
2026-09-06 01:29:07 +0330

    fix(mainui): import quiz layout weight

--- main-ui/src/main/kotlin/com/asdevelopers/academy/mainui/AcademyTrackedQuizScreen.kt
+++ main-ui/src/main/kotlin/com/asdevelopers/academy/mainui/AcademyTrackedQuizScreen.kt
@@ -3,6 +3,7 @@ package com.asdevelopers.academy.mainui
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.weight
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
