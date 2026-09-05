84a7dce11eccb357dbf1f2ca8ce9f521b0b61a1f
waxew <4622.ar@gmail.com>
2026-09-06 00:57:02 +0330

    build(mainui): add back handling and unit test dependencies

--- main-ui/build.gradle.kts
+++ main-ui/build.gradle.kts
@@ -32,10 +32,13 @@ dependencies {
     }
 
     implementation("androidx.core:core-ktx:1.17.0")
+    implementation("androidx.activity:activity-compose:1.12.2")
     implementation(platform("androidx.compose:compose-bom:2025.12.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.foundation:foundation")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.material:material-icons-extended")
+
+    testImplementation("junit:junit:4.13.2")
     debugImplementation("androidx.compose.ui:ui-tooling")
 }
